861a6e83652dd85293b0ffd65980011f99d44105
tanushjain.dev <tanushjain0610@gmail.com>
2026-02-04 18:59:45 +0530

    Revert "Apigateway"

--- MoneyOps/backend/src/main/java/com/moneyops/invoices/entity/InvoiceStatus.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/com/moneyops/invoices/entity/InvoiceStatus.java
-package com.moneyops.invoices.entity;
-
-public enum InvoiceStatus {
-    DRAFT,
-    SENT,
-    PAID,
-    OVERDUE
-}
